cc84522ed5278a14314cf742b80396a249b2262e
Usualdosage <matt@usualdosage.com>
2022-06-13 12:09:39 -0400

    Testing api.

--- pages/api/listMaps.js
+++ pages/api/listMaps.js
@@ -2,16 +2,19 @@ const fs = require("fs");
 const folder = './public/maps'
 
 export default function handler(req, res) {
-  fs.readdir(folder, (err, files) => {
-    if (err) {
-      console.log(err);
-    }
-    else if (!files) {
-      console.log("No map files found at path.")
-      return files;
-    }
-    else {
-      res.status(200).json(files)
-    }
-  });
+  // fs.readdir(folder, (err, files) => {
+  //   if (err) {
+  //     console.log(err);
+  //   }
+  //   else if (!files) {
+  //     console.log("No map files found at path.")
+  //     return files;
+  //   }
+  //   else {
+  //     res.status(200).json(files)
+  //   }
+  // });
+  console.log('Entered the serverless function')
+
+    return { "dummy": "data"}
 }
