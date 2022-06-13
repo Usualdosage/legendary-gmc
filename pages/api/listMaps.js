eb52b7a1d10af158797593c9f0784663df28d999
Usualdosage <matt@usualdosage.com>
2022-06-13 12:19:09 -0400

    Restored API.

--- pages/api/listMaps.js
+++ pages/api/listMaps.js
@@ -1,24 +1,17 @@
 const fs = require("fs");
 const folder = './public/maps'
 
-// export default function handler(req, res) {
-//   // fs.readdir(folder, (err, files) => {
-//   //   if (err) {
-//   //     console.log(err);
-//   //   }
-//   //   else if (!files) {
-//   //     console.log("No map files found at path.")
-//   //     return files;
-//   //   }
-//   //   else {
-//   //     res.status(200).json(files)
-//   //   }
-//   // });
-//   console.log('Entered the serverless function')
-
-//     return { "dummy": "data"}
-// }
-
-export default function handler(request, response) {
-  response.status(200).send(`This should list maps.`);
+export default function handler(req, res) {
+  fs.readdir(folder, (err, files) => {
+    if (err) {
+      console.log(err);
+    }
+    else if (!files) {
+      console.log("No map files found at path.")
+      return files;
+    }
+    else {
+      res.status(200).json(files)
+    }
+  });
 }
